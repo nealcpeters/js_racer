1f87f71d3b8c87896c59211ba7bbd0ff403f1220
To-mos <tomcatd10@gmail.com>
2014-02-25 10:02:27 -0600

    stuff

--- public/js/application.js
+++ public/js/application.js
@@ -16,43 +16,52 @@
 //   time: new Date().getTime()
 // };
 
-function initializeBoard(){
+function initializeBoard() {
   $('#player1_won').hide();
   $('#player2_won').hide();
   $('form').hide();
 };
 
-$(document).ready(function() {
+function move_player(player_id, winner) {
+  if (!winner) {
+    $(player_id).find(".active").removeClass('active').next().addClass('active');
 
+    if ($('.active').hasClass('last')) {
+      winner = true;
+      id = "";
+      if (player_id === "#player1_strip")
+        id = "#player1_won";
+      else
+        id = "#player2_won";
 
-  winner = false;
-  function move_player(players_id) {
-      if (!winner) {
-        $(players_id).find(".active").removeClass('active').next().addClass('active');
-
-        if ($('.active').hasClass('last')) {
-          winner = true;
-          id = "";
-          if (players_id === "#player1_strip")
-            id = "#player1_won";
-          else
-            id = "#player2_won";
-
-          $(id).show();
-          $('form').show();
-          $('#return_home').val(id);
-      }
+      $(id).show();
+      $('form').show();
+      $('#return_home').val(id);
     }
   }
+}
 
+
+
+function movement() {
   $(document).on("keyup", function(evt) {
-    evt = evt || window.event;
-    var charCode = evt.keyCode || evt.which;
-    var charStr = String.fromCharCode(charCode);
-    if (charStr === "K")
-      move_player("#player1_strip")
+      evt = evt || window.event;
+      var charCode = evt.keyCode || evt.which;
+      var charStr = String.fromCharCode(charCode);
+      if (charStr === "K")
+        move_player("#player1_strip", winner)
+      else if (charStr === "S")
+        move_player("#player2_strip", winner)
+    });
+};
 
-    else if (charStr === "S")
-      move_player("#player2_strip")
-  });
+
+$(document).ready(function() {
+  initializeBoard();
+
+  winner = false;
+
+  movement();
+
+
 });
